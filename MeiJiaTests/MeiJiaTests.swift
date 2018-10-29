4337fd14ef836d517469c76726ee21664b13db03
江东 <j309999174@jiangdongdemac.local>
2018-10-29 14:34:22 -0700

    Initial Commit

---
+++ MeiJiaTests/MeiJiaTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeiJiaTests.swift
+//  MeiJiaTests
+//
+//  Created by 江东 on 2018/10/29.
+//  Copyright © 2018 oushelun. All rights reserved.
+//
+
+import XCTest
+@testable import MeiJia
+
+class MeiJiaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
