a8e6cbba1c120145db3242d0556069efaf88dbb9
江东 <j309999174@jiangdongdemac.local>
2018-10-29 23:39:14 -0700

    美嘉商城1.0

--- MeiJia/AppDelegate.swift
+++ MeiJia/AppDelegate.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
-
+import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //打开音乐，播放音乐，为了保持后台
+    let queue = DispatchQueue(label: "创建并行队列", attributes: .concurrent)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //请求通知权限
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) {
+                (accepted, error) in
+                if !accepted {
+                    print("用户不允许消息通知。")
+                }
+        }
         return true
     }
 
@@ -27,6 +37,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        queue.async {
+            //通知内容
+            let content = UNMutableNotificationContent()
+            content.title = "欢迎您使用美嘉线上服务应用"
+            content.body = "各种线上活动火热进行中，详情可向客服人员咨询！！！"
+            //设置通知触发器
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
+            //设置请求标识符
+            let requestIdentifier = "1"
+            //设置一个通知请求
+            let request = UNNotificationRequest(identifier: requestIdentifier,content: content, trigger: trigger)
+            //将通知请求添加到发送中心
+            UNUserNotificationCenter.current().add(request) { error in
+                if error == nil {
+                    print("Time Interval Notification scheduled: \(requestIdentifier)")
+                }
+            }
+            print("1 秒后输出")
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
